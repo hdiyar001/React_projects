8d9c275de370c8154f91ce61bc1f16c6e7365707
janalo <janalo957@gmail.com>
2023-08-29 23:05:03 +0200

    Login Basics

--- my-first-project/src/Components/LoginItem.js
+++ my-first-project/src/Components/LoginItem.js
@@ -1,4 +1,4 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import LoginItem from "./LoginItem";
 
@@ -7,16 +7,34 @@ const Login = () => {
     ["Diyar", "1234"],
     ["Jano", "4325"],
   ];
+
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  const userNameHandler = (id ,ChangeHandler) => {
+    if(id == "username"){
+      setUsername(ChangeHandler.target.value);
+    } else {
+      setPassword(ChangeHandler.target.value);
+    }
+  }
+
+
   const submitHandler = (event) => {
     event.preventDefault();
-    console.log(event.target.value);
+    
+    console.log(username);
+    console.log(password);
+
   };
   return (
     <form onSubmit={submitHandler}>
       <p>username</p>
-      <input type="text"></input>
+      <input type="text" onChange={event => userNameHandler("username",event)}></input>
       <p>password</p>
-      <input type="text"></input>
+      <input type="text" onChange={event => userNameHandler("password",event)}></input>
       <br />
       <button type="submit">Submit</button>
     </form>

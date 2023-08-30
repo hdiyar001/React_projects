5adb79e8c487f68daf28714751ea3cfb3bb9991a
HDiyar <hdiyar998@gmail.com>
2023-08-30 20:08:54 +0200

    new

--- my-first-project/src/Components/LoginItem.js
+++ my-first-project/src/Components/LoginItem.js
@@ -9,33 +9,35 @@ const Login = () => {
     ["Jano", "4325"],
   ];
 
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-
-  const userNameHandler = (id ,ChangeHandler) => {
-    if(id == "username"){
+  const userNameHandler = (id, ChangeHandler) => {
+    if (id == "username") {
       setUsername(ChangeHandler.target.value);
     } else {
       setPassword(ChangeHandler.target.value);
     }
-  }
-
+  };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    
+
     console.log(username);
     console.log(password);
-
   };
   return (
     <form onSubmit={submitHandler}>
       <p>username</p>
-      <input type="text" onChange={event => userNameHandler("username",event)}></input>
+      <input
+        type="text"
+        onChange={(event) => userNameHandler("username", event)}
+      ></input>
       <p>password</p>
-      <input type="text" onChange={event => userNameHandler("password",event)}></input>
+      <input
+        type="text"
+        onChange={(event) => userNameHandler("password", event)}
+      ></input>
       <br />
       <button type="submit">Submit</button>
     </form>

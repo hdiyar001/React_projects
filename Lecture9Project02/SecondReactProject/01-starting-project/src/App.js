eae0687c9fcd2d933c40fa34bc119b3d4e49684b
HDiyar <hdiyar998@gmail.com>
2023-09-11 20:08:16 +0200

    App.js refactoring

--- Lecture9Project02/SecondReactProject/01-starting-project/src/App.js
+++ Lecture9Project02/SecondReactProject/01-starting-project/src/App.js
@@ -1,30 +1,31 @@
 import { useState } from "react";
-
 import UserOutput from "./Components/UserOutput";
 import ExceptionHandlingWindow from "./Components/ExceptionHandlingWindow";
 import UserInput from "./Components/UserInput";
 
 function CheckValidation(data) {
-  return !(data.userNameVar < 1 || !data);
+  return data && data.userNameVar >= 1;
 }
+
 function App() {
-  const [tryAddUser, setTryAddUser] = useState(true);
-  const [redirectUserInupts, setRedirectUserInupts] = useState([]);
+  const [tryAddUser, setTryAddUser] = useState(true); // Initialize as false
+  const [redirectUserInputs, setRedirectUserInputs] = useState({});
 
   const redirectData = (value) => {
     console.log(value);
-    // if (CheckValidation(value)) {
-    //   setTryAddUser(true);
-    //   setRedirectUserInupts(value);
-    // } else {
-    //   setTryAddUser(false);
-    // }
+    if (CheckValidation(value)) {
+      setTryAddUser(true);
+      setRedirectUserInputs(value);
+    } else {
+      setTryAddUser(false);
+    }
   };
+
   return (
     <div>
       <UserInput userInfo={redirectData} />
-      {/* {tryAddUser && <UserOutput />}
-      {!tryAddUser && <ExceptionHandlingWindow />} */}
+      {tryAddUser && <UserOutput />}
+      {!tryAddUser && <ExceptionHandlingWindow />}
     </div>
   );
 }
